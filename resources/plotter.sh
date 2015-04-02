a761dc83c3f999297688c95cc31830fdf287199c
Jefferson Chaves Gomes <jefferson.gomes@brisa.org.br>
2015-04-01 22:48:45 -0300

    Parcial implementation

---
+++ resources/plotter.sh
@@ -0,0 +1,97 @@
+# !bin/bash
+
+OUTPUT_TEMP="data.dat"
+
+# HELP
+Help() {
+
+echo "Help: ${0##*/} [-h] [ [-g tipo] [-x coluna] -y coluna[:coluna2] -f filtro -o entrada saida ]
+Onde:
+-h[H]          = Mostra esta tela de ajuda.
+-x[X] coluna   = Coluna do arquivo a ser utilizado para o campo X
+-y[Y] coluna   = Coluna do arquivo a ser utilizado para o campo Y pode ser usado mais de um separado por ':'
+-f[F] filtro   = Expressão regular de filtro a ser utilizado
+-g[G] tipo     = Tipo de gráfico a ser gerado (lines, boxes)
+entrada        = Arquivo de entrada   
+saida          = Imagem PNG de saída 
+
+Ex.: ${0##*/} -f \"\\[LAYER\\]\" -g lines -x 1 -y 5:6 teste.log grafico.png 
+     
+";}
+
+# Checando parâmetros:
+[[ $BASH_ARGC ]] || { echo "Faça: '${0##*/} -h' para obter ajuda."; exit 1; }
+
+while getopts "hHo:O:f:F:x:X:y:Y:g:G:" OPC 2>/dev/null
+do
+   case "$OPC" in
+      [gG]) TYPE="$OPTARG" ;;
+      [fF]) FILTER="$OPTARG" ;;
+      [xX]) X="$OPTARG"
+            [[ $X != [0-9]* ]] && echo "Parâmetro X incorreto" && Help;;
+      [yY]) Y="$OPTARG";;
+      [hH]) Help ; exit ;;
+      *) echo "Faça: '${0##*/} -h' para obter ajuda."; exit 1 ;;
+#      [eE]) EXT="$OPTARG"
+#      [[ ${EXT##.*} == $EXT ]] && EXT=".$OPTARG" ;;
+    esac
+done
+
+
+
+# Removendo opções para definir arquivos de entrada e saida
+shift $(($OPTIND-1))
+ 
+if [ -z "$1" ]; then
+    echo "O arquivo de entrada e de saida devem ser informados." && exit 1;
+fi
+
+if [ -z "$2" ] ; then
+    echo "O arquivo de saida deve ser informado." && exit 1;
+fi
+
+
+
+INPUT=$1
+OUTPUT=$2    
+                 
+if [ -z "$X" ]; then
+    X=1    
+fi
+
+if ! [ -z "$TYPE" ] ; then
+    TYPE="with $TYPE"
+fi
+
+cat $INPUT | grep $FILTER | cut -d ' ' -f2- | sed 's/[][]//g'  | sed 's/[a-zA-Z]*://g'| sed 's/;/\t/g' > $OUTPUT_TEMP
+
+XLABEL=`cat $INPUT | grep $FILTER | cut -d ' ' -f2- | sed 's/[][]//g' | cut -d ';' -f$X | cut -d ':' -f1 | head -n 1` 
+
+for YIN in `echo $Y | tr ":" "\n"`
+do
+    YLABEL=`cat $INPUT | grep $FILTER | cut -d ' ' -f2- | sed 's/[][]//g' | cut -d ';' -f$YIN | cut -d ':' -f1 | head -n 1` 
+
+    if ! [ -z "$PLOTCMD" ]; then
+        PLOTCMD=$PLOTCMD,    
+    fi
+
+    PLOTCMD=$PLOTCMD"\"$OUTPUT_TEMP\" using $X:$YIN $TYPE title \"$YLABEL\""
+
+done
+
+PLOTCMD="plot "$PLOTCMD
+
+gnuplot << EOF
+
+set grid
+set xrange [0:]
+set terminal png
+set output '$OUTPUT'
+set xlabel "$XLABEL"
+$PLOTCMD
+
+pause -1
+
+EOF
+
+rm $OUTPUT_TEMP
--- src/br/unb/main.cpp
+++ src/br/unb/main.cpp
@@ -8,13 +8,112 @@
  ============================================================================
  */
 
-#include <stdlib.h>
 #include <iostream>
+#include <set>
+#include <iterator>
+#include <time.h>
 #include <omp.h>
 
-/**
- * Hello OpenMP World prints the number of threads and the current thread id
- */
+// Definitions
+#define MIN_RANGE 1
+#define MAX_RANGE 1000000000
+
+// Enums definitions
+enum OutputType {
+	NONE = 0,
+    TIME = 1,
+    LIST = 2,
+    ALL = 3
+};
+
+// Strutcs definitions
+struct Result {
+    long processTime;
+    std::set<int> lstPrimes;
+};
+
+// Functions declarations
+int readUpperLimit();
+OutputType readOutputType();
+Result findPrimesSequential(int);
+bool isPrime(int num);
+void printResult(OutputType, Result);
+
+// Global variables
+int processTime = -1;
+
+int main(int argc, char *argv[]) {
+	int upperLimit = readUpperLimit();
+	OutputType outputType = readOutputType();
+	Result result = findPrimesSequential(upperLimit);
+	printResult(outputType, result);
+
+
+
+//
+//	for (int i = 2; i < num; i++) {
+//		if (isPrime(i)) {
+//			primesArray[primesSize++] = i;
+//		}
+//	}
+//	for (int i = 0; i < primesSize; i++) {
+//		std::cout << primesArray[i] << " - ";
+//	}
+
+	return EXIT_SUCCESS;
+}
+
+int readUpperLimit() {
+	int num = 0;
+	std::cout << std::endl << "Informe um limite superior da lista de primos a ser impressa: " << std::flush;
+	std::cin >> num;
+	while (!(std::cin.good()) && !(num > MIN_RANGE && num <= MAX_RANGE)) {
+		std::cin.clear();
+		std::cin.ignore(INT_MAX, '\n');
+		std::cout << "Por favor. Informe um numero natural maior que " << MIN_RANGE << " e menor ou igual a " << MAX_RANGE << ": " << std::flush;
+		std::cin >> num;
+	}
+	return num;
+}
+
+OutputType readOutputType() {
+	std::cout << std::endl << "Informe o tipo de saida a ser impressa: " << std::endl << std::flush;
+	std::cout << "Digite: time (para imprimir somente o tempo de processamento)" << std::endl << std::flush;
+	std::cout << "Digite: list (para imprimir somente a lista de numeros primos)" << std::endl << std::flush;
+	std::cout << "Digite: all  (para imprimir o tempo e a lista citados acima)" << std::endl << std::flush;
+	OutputType outputType = NONE;
+	std::string input = "";
+	while(true) {
+		std::cin >> input;
+		if (input == "time") {
+			outputType = TIME;
+		} else if (input == "list") {
+			outputType = LIST;
+		} else if (input == "all") {
+			outputType = ALL;
+		}
+		if (outputType != NONE) {
+			break;
+		}
+		std::cin.clear();
+		std::cin.ignore(INT_MAX, '\n');
+		std::cout << "Valor incorreto! Informe novamente:" << std::endl << std::flush;
+	}
+	return outputType;
+}
+
+Result findPrimesSequential(int upperLimit) {
+	Result result;
+	clock_t startTime = clock();
+	for (int i = 2; i < upperLimit; i++) {
+		if (isPrime(i)) {
+			result.lstPrimes.insert(i);
+		}
+	}
+	clock_t endTime = clock();
+	result.processTime = (endTime - startTime) / CLOCKS_PER_SEC;
+	return result;
+}
 
 bool isPrime(int num) {
 	for (int i = 2; i < num; ++i) {
@@ -25,25 +124,20 @@ bool isPrime(int num) {
 	return true;
 }
 
+void printResult(OutputType outputType, Result result) {
+	switch (outputType) {
+		case TIME:
 
-int main(void) {
+			break;
+		case LIST:
+			for (std::set<int>::iterator element = result.lstPrimes.begin(); element != result.lstPrimes.end(); element++) {
+				std::cout << *element << " ";
+			}
+			break;
+		case ALL:
 
-	int num = 2;
-	std::cout << "Informe um número natural maior que 1: ";
-	std::cin >> num;
-
-	int primesSize = 0;
-	int primesArray[num];
-	for (int i = 2; i < num; i++) {
-		if (isPrime(i)) {
-			primesArray[primesSize++] = i;
-		}
+			break;
+		default:
+			break;
 	}
-
-	for (int i = 0; i < primesSize; i++) {
-		std::cout << primesArray[i] << " - ";
-	}
-
-	return EXIT_SUCCESS;
 }
-
